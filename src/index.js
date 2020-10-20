16f93f368b58aa816c1cdcffabb6476b1421d6e4
antipea <71250351+antipea@users.noreply.github.com>
2020-10-20 15:08:51 +0300

    feat: add common function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n).toString();
+    let newN = '';
+    for ( let i = 0; i < n.length; i++) {
+        newN = `${n[i]}${newN}`;
+    }
+    newN = Number(newN);
+    return newN;
 }
